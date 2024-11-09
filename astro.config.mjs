7818631873eaf357d3d6623a0866e67452d44d23
nspo01634 <nspo01634@gmail.com>
2024-11-09 17:00:07 +0000

    workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
+    site: 'https://landing.stardbots.net',
+    base: '',
     integrations: [react(), tailwind()],
 });
